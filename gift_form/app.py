473ffdf1502a0888b388370aa7639f8f2664c581
book31014-cmd <book31014@gmail.com>
2025-12-15 02:13:38 +0800

    Update app.py

--- gift_form/app.py
+++ gift_form/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import sqlite3
 from datetime import datetime
@@ -88,6 +89,7 @@ if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
