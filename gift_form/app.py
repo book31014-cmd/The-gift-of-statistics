ecca2cdca7ae8d1e4b60629cd14dfbf1cf84a581
book31014-cmd <book31014@gmail.com>
2025-12-15 02:11:34 +0800

    Update app.py

--- gift_form/app.py
+++ gift_form/app.py
@@ -82,8 +82,12 @@ def admin():
     return html
 
 # ---------- 啟動 ----------
+import os
+
 if __name__ == "__main__":
     init_db()
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
